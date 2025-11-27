52af7c321ae8415278d2411954916fd8d3433225
mokashiif <m.kashifbaigal@gmail.com>
2025-11-27 15:12:59 +0500

    hh

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,6 +2,6 @@ function searchLyrics() {
     let text = document.getElementById("searchInput").value;
 
     document.getElementById("resultBox").innerHTML =
-        "You searched for: " + text;
+        "<strong>You searched for:</strong> " + text;
 }
 
